57af442fc95556c9d2f60e86ab0053d9f280e342
Serge Ntumba <s7ntumba@gmail.com>
2022-04-22 23:57:22 +0100

    navigation

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "react-native-gesture-handler";
 import HomeScreen from "./src/screens/HomeScreen";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import RootNavigator from "./src/navigations/RootNavigators";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
+    <RootNavigator />
+    // <View style={styles.container}>
+    //   <HomeScreen />
+    // </View>
   );
 };
 
--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -1,13 +1,31 @@
 import * as React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+
 import { HomeStack } from "./StackNavigation";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Icon } from "@rneui/base";
+import { colors, Colors } from "../global/styles";
 
 const Drawer = createDrawerNavigator();
 
-export default function createDrawerNavigator() {
+export default function DrawerNavigator() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="HomeStack" component={HomeStack} />
+      <Drawer.Screen
+        name="HomeStack"
+        component={HomeStack}
+        options={{
+          title: "Home",
+          drawerIcon: ({ focussed, size }) => (
+            <Icon
+              type="material-community"
+              name="Home"
+              color={focussed ? "#7cc" : colors.grey2}
+              size={size}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
     </Drawer.Navigator>
   );
 }
